8a7a6ac2da82159f31a1e69ac473d4f456deb2e5
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-02-24 22:25:26 +0100

    Refactor to work with new units

--- src/scripts/weatherData.js
+++ src/scripts/weatherData.js
@@ -1,11 +1,9 @@
 const API_KEY = "1541647ca6a09c8e891e90c3eb2ff7f7";
-const MEASUREMENT_UNIT = "metric";
 const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?";
 
 const LOCATION_URL = "http://api.openweathermap.org/geo/1.0/direct?";
 
 const fetchUserLocation = async (location) => {
-
   try {
     const response = await fetch(
       `${LOCATION_URL}q=${location}}&appid=${API_KEY}&limit=1`
@@ -15,22 +13,20 @@ const fetchUserLocation = async (location) => {
 
     const { lat, lon, name } = data[0];
 
-    return { lat, lon, name }
-    
+    return { lat, lon, name };
   } catch (err) {
     console.log(err);
     alert(err);
   }
 };
 
-const fetchWeatherData = async (location) => {
+const fetchWeatherData = async (location, unit) => {
   try {
     const response = await fetch(
-      `${WEATHER_URL}appid=${API_KEY}&lon=${location.lon}&lat=${location.lat}&units=${MEASUREMENT_UNIT}`
+      `${WEATHER_URL}appid=${API_KEY}&lon=${location.lon}&lat=${location.lat}&units=${unit}`
     );
     const data = await response.json();
-    return data;
-
+    return data
   } catch (err) {
     console.log(err);
     alert(err);
