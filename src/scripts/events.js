c3bba486f7c47187c6d622acdeee933a4ec870f8
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-02-24 22:24:41 +0100

    Add unit switching and refactor switch button classes

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -2,17 +2,34 @@ import setWeatherData from "./setData";
 
 const events = (function () {
   const form = document.getElementById("form");
+  const switchBtn = document.getElementById("switchButton");
+
+  let baseCity = "Warsaw";
+  let baseUnit = "metric";
 
   form.addEventListener("submit", (e) => {
     const searchValue = document.getElementById("searchBar").value;
 
-    e.preventDefault()
-    setWeatherData(searchValue);
-    form.reset()
+    e.preventDefault();
+
+    baseCity = searchValue;
+    setWeatherData(searchValue, baseUnit);
+
+    form.reset();
+  });
+
+  switchBtn.addEventListener("click", () => {
+    switchBtn.checked
+      ? (switchBtn.dataset.unit = "standard")
+      : (switchBtn.dataset.unit = "metric");
+
+    baseUnit = switchBtn.dataset.unit;
+
+    setWeatherData(baseCity, baseUnit);
   });
 
   document.addEventListener("DOMContentLoaded", () => {
-    setWeatherData("London");
+    setWeatherData(baseCity, baseUnit);
   });
 })();
 
